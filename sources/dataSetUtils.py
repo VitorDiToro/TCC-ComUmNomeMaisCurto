6c8f9c7460abf4c8f710e1e38fc098749c6b9888
VitorDiToro <vitorrditoro@gmail.com>
2018-05-19 16:53:04 -0300

    updated dataSetUtils.py

--- sources/dataSetUtils.py
+++ sources/dataSetUtils.py
@@ -1,29 +1,106 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# Implementação do KNN
+# Implementação do CU DA SUA MÃE!
+#   Brincadeira, é a implementação de funções úteis p/ manipular o DataSet.
 
 # Author        :   Vitor Rodrigues Di Toro
 # E-Mail        :   vitorrditoro@gmail.com
 # Date          :   23/03/2018
-# Last Update   :   27/03/2018
+# Last Update   :   19/05/2018
+
+import csv
+from random import shuffle
 
 import sys
 sys.path.append('../')
 
-import csv
 
-with open('../dataset/ionosphere.data','r') as File:
-    reader = csv.reader(File, delimiter=',', quotechar=',',
-                        quoting=csv.QUOTE_MINIMAL)
+def num(s):
+    try:
+        return float(s)
+    except ValueError:
+        return s
 
-    l_reader = list(reader)
 
-mycsv_list = []    
-for i in range(len(l_reader)):
-    if not(l_reader[i] in l_reader[i+1:len(l_reader)]):
-        mycsv_list.append(l_reader[i])
+class DataSet:
+    @staticmethod
+    def fix_data_set(data_set_name, extension):
 
-with open("../dataset/ionosphere.csv", "w") as File:
-    writer = csv.writer(File, lineterminator='\n')
-    writer.writerows(mycsv_list)
+        with open('../dataset/' + data_set_name + "." + extension, 'r') as File:
+            reader = csv.reader(File, delimiter=',', quotechar=',', quoting=csv.QUOTE_MINIMAL)
+
+            l_reader = list(reader)
+
+        my_csv_list = []
+        for i in range(len(l_reader)):
+            if not(l_reader[i] in l_reader[i+1:len(l_reader)]):
+                my_csv_list.append(l_reader[i])
+
+        with open("../dataset/" + data_set_name + ".csv", "w") as File:
+            writer = csv.writer(File, lineterminator='\n')
+            writer.writerows(my_csv_list)
+
+    @staticmethod
+    def get_data_lc(data_set_name, lines, columns, randomize=False):
+        with open(data_set_name, 'r') as File:
+            reader = csv.reader(File, delimiter=',', quotechar=',', quoting=csv.QUOTE_MINIMAL)
+
+            l_reader = list(reader)
+
+            if randomize:
+                shuffle(l_reader)
+
+        data = []
+        tmp = [[l_reader[i][j] for j in columns] for i in lines]
+        for row in tmp:
+            data.append([num(i) for i in row])
+
+        return data
+
+    @staticmethod
+    def get_data(dataset_name: str='ionosphere.csv', percent_to_training: int=60, randomize: bool=True, verbose: bool=True):
+        # TODO --> Fix DocString
+        """
+        :param dataset_name:
+        :param percent_to_training:
+        :param randomize:
+        :param verbose:
+        :return:
+        """
+        count = 0
+        group_g = 0
+        test_data = []
+        training_data = []
+
+        with open(dataset_name, 'r') as File:
+            reader = csv.reader(File, delimiter=',', quotechar=',', quoting=csv.QUOTE_MINIMAL)
+
+            l_reader = list(reader)
+            limit = int(len(l_reader) * (percent_to_training / 100))
+
+            if randomize:
+                shuffle(l_reader)
+
+            for row in l_reader:
+
+                if row[-1] == "g":
+                    group_g += 1
+
+                if count < limit:
+                    training_data.append([num(i) for i in row])
+                else:
+                    test_data.append([num(i) for i in row])
+
+                count += 1
+
+        if verbose:
+            print("Total de amostras: %d:" % len(l_reader))
+            print("    - %d amostra do tipo \"Good\"" % group_g)
+            print("    - %d amostra do tipo \"Bad\"" % (len(l_reader) - group_g))
+            print("")
+            print("%d %% das amostras separadas para treino." % percent_to_training)
+            print("    - %d amostras para treino" % (len(training_data)))
+            print("    - %d amostras para teste" % (len(test_data)))
+
+        return training_data, test_data
